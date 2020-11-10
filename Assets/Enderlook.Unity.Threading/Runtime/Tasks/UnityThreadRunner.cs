a848e3d96b172d97b84b95e90131b01d07455d50
Enderlook <enderlook@gmail.com>
2020-11-10 12:24:49 -0300

    Add UnityThreadRunner and UnityThreadTaskScheduler

---
+++ Assets/Enderlook.Unity.Threading/Runtime/Tasks/UnityThreadRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Enderlook.Unity.Threading.Tasks
+{
+    /// <summary>
+    /// Helper methods for tasks.
+    /// </summary>
+    public static class UnityThreadRunner
+    {
+        private static readonly TaskFactory taskFactory = new TaskFactory(UnityThreadTaskScheduler.Instance);
+
+        public static void Run(Action<object> action, object state, TaskCreationOptions creationOptions) => taskFactory.StartNew(action, state, creationOptions).GetAwaiter().GetResult();
+
+        public static void Run(Action<object> action, object state)
+            => taskFactory.StartNew(action, state).GetAwaiter().GetResult();
+
+        public static void Run(Action action, TaskCreationOptions creationOptions)
+            => taskFactory.StartNew(action, creationOptions).GetAwaiter().GetResult();
+
+        public static void Run(Action action, CancellationToken cancellationToken)
+            => taskFactory.StartNew(action, cancellationToken).GetAwaiter().GetResult();
+
+        public static void Run(Action action)
+            => taskFactory.StartNew(action).GetAwaiter().GetResult();
+
+        public static TResult Run<TResult>(Func<TResult> function)
+            => taskFactory.StartNew(function).GetAwaiter().GetResult();
+
+        public static void Run(Action<object> action, object state, CancellationToken cancellationToken)
+            => taskFactory.StartNew(action, state, cancellationToken).GetAwaiter().GetResult();
+    }
+}
---
+++ Assets/Enderlook.Unity.Threading/Runtime/Tasks/UnityThreadTaskScheduler.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Unity.Jobs.LowLevel.Unsafe;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Threading.Tasks
+{
+    /// <summary>
+    /// Defines an shcheduler which runs all its tasks in the main thread.
+    /// </summary>
+    public sealed class UnityThreadTaskScheduler : TaskScheduler
+    {
+        public static UnityThreadTaskScheduler Instance = new UnityThreadTaskScheduler();
+
+        public override int MaximumConcurrencyLevel => 1;
+
+        protected override void QueueTask(Task task) => TryExecuteTaskMainThread(task);
+
+        protected override bool TryExecuteTaskInline(
+            Task task,
+            bool taskWasPreviouslyQueued) => TryExecuteTaskMainThread(task);
+
+        private bool TryExecuteTaskMainThread(Task task)
+        {
+            if (ThreadSwitcher.IsExecutingMainThread)
+                return TryExecuteTask(task);
+            else
+            {
+                if (JobsUtility.IsExecutingJob)
+                {
+                    if (Application.platform == RuntimePlatform.WebGLPlayer)
+                    {
+                        // We are in a problem. WebGL doesn't have multithreading, and so we can't run a task to run it.
+                        Debug.LogWarning("Attempted to run in main thread from a Unity Job in WebGL platform. Due lack of multithreading a fallback will run this code and all subsequent code of the job in the main thread.");
+                        return SwitchInSiteNoReturn(task).GetAwaiter().GetResult();
+                    }
+                    else
+                        return Task.Run(async () =>
+                        {
+                            await ThreadSwitcher.ResumeUnityAsync;
+                            return TryExecuteTask(task);
+                        }).GetAwaiter().GetResult();
+                }
+                else
+                    return SwitchInSite(task).GetAwaiter().GetResult();
+            }
+        }
+
+        private async ValueTask<bool> SwitchInSiteNoReturn(Task task)
+        {
+            await ThreadSwitcher.ResumeUnityAsync;
+            return TryExecuteTask(task);
+        }
+
+        private async ValueTask<bool> SwitchInSite(Task task)
+        {
+            await ThreadSwitcher.ResumeUnityAsync;
+            bool result = TryExecuteTask(task);
+            await ThreadSwitcher.ResumeTaskAsync;
+            return result;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks() => Enumerable.Empty<Task>();
+    }
+}
