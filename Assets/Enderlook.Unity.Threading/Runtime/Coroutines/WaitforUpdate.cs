9bd1ee76d046a1c056ee2d4cd8955ba408dbe660
Enderlook <enderlook@gmail.com>
2020-11-10 08:19:16 -0300

    Add Awaiters

---
+++ Assets/Enderlook.Unity.Threading/Runtime/Coroutines/WaitforUpdate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Enderlook.Unity.Threading.Coroutines
+{
+    /// <summary>
+    /// Wait until the next frame, same as passing <see langword="null"/> to a coroutine.
+    /// </summary>
+    public sealed class WaitforUpdate : CustomYieldInstruction
+    {
+        // https://github.com/svermeulen/Unity3dAsyncAwaitUtil/blob/master/UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/WaitForUpdate.cs
+
+        public override bool keepWaiting => false;
+    }
+}
