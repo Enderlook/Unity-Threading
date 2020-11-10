84955685ba1a4113d17514361f437ae4ccb098a2
Enderlook <enderlook@gmail.com>
2020-11-10 08:49:51 -0300

    Add UnitySynchronizationContextUtility

---
+++ Assets/Enderlook.Unity.Threading/Runtime/Threading/UnitySynchronizationContextUtility.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+
+using UnityEngine;
+
+namespace Assets.Enderlook.Unity.Threading
+{
+    /// <summary>
+    /// A helper class to gather the thread id and synchronization context of Unity main thread.
+    /// </summary>
+    public static class UnitySynchronizationContextUtility
+    {
+        /// <summary>
+        /// Synchronization context used by Unity.
+        /// </summary>
+        public static SynchronizationContext UnitySynchronizationContext { get; private set; }
+
+        /// <summary>
+        /// Thread Id used by Unity main thread.
+        /// </summary>
+        public static int UnityThreadId { get; private set; }
+
+        // https://github.com/svermeulen/Unity3dAsyncAwaitUtil/blob/master/UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            UnitySynchronizationContext = SynchronizationContext.Current;
+            UnityThreadId = Thread.CurrentThread.ManagedThreadId;
+        }
+    }
+}
