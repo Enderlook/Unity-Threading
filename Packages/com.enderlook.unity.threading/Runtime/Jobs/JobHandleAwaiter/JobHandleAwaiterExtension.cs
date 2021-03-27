67c19fa483d2164962d80911d126c8e6253f313a
Enderlook <enderlook@gmail.com>
2021-03-27 19:40:08 -0300

    Remove unnecessary extension methods

--- Packages/com.enderlook.unity.threading/Runtime/Jobs/JobHandleAwaiter/JobHandleAwaiterExtension.cs
+++ Packages/com.enderlook.unity.threading/Runtime/Jobs/JobHandleAwaiter/JobHandleAwaiterExtension.cs
@@ -1,8 +1,4 @@
-﻿using Enderlook.Unity.Coroutines;
-
-using System.Collections;
-
-using Unity.Jobs;
+﻿using Unity.Jobs;
 
 namespace Enderlook.Unity.Jobs
 {
@@ -17,22 +13,5 @@ namespace Enderlook.Unity.Jobs
         /// <param name="jobHandle">Job handle to wrap.</param>
         /// <returns>Task wrapper of the job handle.</returns>
         public static JobHandleAwaiter GetAwaiter(this JobHandle jobHandle) => new JobHandleAwaiter(jobHandle);
-
-        /// <summary>
-        /// Convert a job handle into an enumerator.
-        /// </summary>
-        /// <param name="jobHandle">Job handle to convert.</param>
-        /// <returns>Enumerator which wraps the job handle.</returns>
-        public static IEnumerator AsCoroutine(this JobHandle jobHandle)
-        {
-            while (!jobHandle.IsCompleted)
-                yield return null;
-
-            jobHandle.Complete();
-        }
-
-        /// <inheritdoc cref="WaitForJobComplete.Create(JobHandle)"/>
-        public static WaitForJobComplete Wait(this JobHandle handle)
-            => WaitForJobComplete.Create(handle);
     }
 }
