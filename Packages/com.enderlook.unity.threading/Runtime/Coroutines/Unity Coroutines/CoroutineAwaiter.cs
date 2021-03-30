f2dfaf747a6ee864fc49e50f64e12c541cafdb87
Enderlook <enderlook@gmail.com>
2021-03-30 15:45:57 -0300

    Add CoroutineAwaiter

---
+++ Packages/com.enderlook.unity.threading/Runtime/Coroutines/Unity Coroutines/CoroutineAwaiter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace Enderlook.Unity.Coroutines
+{
+    /// <summary>
+    /// An awaiter for <see cref="UnityEngine.Coroutine"/>
+    /// </summary>
+    public readonly struct CoroutineAwaiter : INotifyCompletion
+    {
+        private readonly Handle handle;
+
+        private class Handle
+        {
+            public Action onCompleted;
+
+            public bool isCompleted;
+        }
+
+        /// <summary>
+        /// Creates an awaiter for the given <see cref="JobHandle"/>.
+        /// </summary>
+        /// <param name="coroutine">Job handle to await for.</param>
+        public CoroutineAwaiter(UnityEngine.Coroutine coroutine)
+        {
+            Handle handle = new Handle();
+            this.handle = handle;
+            Coroutine.Unity.Start(Work());
+            IEnumerator Work()
+            {
+                yield return coroutine;
+                handle.isCompleted = true;
+                handle.onCompleted?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Return self.
+        /// </summary>
+        /// <returns>Self.</returns>
+        public CoroutineAwaiter GetAwaiter() => this;
+
+        /// <summary>
+        /// Whenever the job handle has completed or not.
+        /// </summary>
+        public bool IsCompleted => handle.isCompleted;
+
+        /// <summary>
+        /// Determines the action to run after the job handle has completed.
+        /// </summary>
+        /// <param name="continuation">Action to run.</param>
+        public void OnCompleted(Action continuation)
+        {
+            if (IsCompleted)
+                continuation();
+            else
+                handle.onCompleted += continuation;
+        }
+
+        /// <summary>
+        /// Does nothing.
+        /// </summary>
+        public void GetResult() { }
+    }
+}
---
+++ Packages/com.enderlook.unity.threading/Runtime/Coroutines/Unity Coroutines/CoroutineAwaiterExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Enderlook.Unity.Coroutines
+{
+    /// <summary>
+    /// Allow get awaiter from <see cref="UnityEngine.Coroutine"/>.
+    /// </summary>
+    public static class CoroutineAwaiterExtension
+    {
+        /// <summary>
+        /// Convert a <see cref="UnityEngine.Coroutine"/> to a task.
+        /// </summary>
+        /// <param name="coroutine">Coroutine to convert.</param>
+        /// <returns>Task wrapper of a Unity coroutine.</returns>
+        public static CoroutineAwaiter GetAwaiter(this UnityEngine.Coroutine coroutine) => new CoroutineAwaiter(coroutine);
+    }
+}
