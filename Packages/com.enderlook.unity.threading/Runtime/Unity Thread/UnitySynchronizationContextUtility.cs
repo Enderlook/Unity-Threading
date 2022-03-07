6660730eecc87376089eea2c79984a08d7c0971e
Enderlook <enderlook@gmail.com>
2022-03-07 15:38:12 -0300

    Improve safety of UnitySynchronizationContextUtility

--- Packages/com.enderlook.unity.threading/Runtime/Unity Thread/UnitySynchronizationContextUtility.cs
+++ Packages/com.enderlook.unity.threading/Runtime/Unity Thread/UnitySynchronizationContextUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 using UnityEditor;
@@ -15,38 +16,64 @@ namespace Enderlook.Unity.Threading
         /// <summary>
         /// Synchronization context used by Unity.
         /// </summary>
-        public static SynchronizationContext UnitySynchronizationContext { get; private set; }
+        public static SynchronizationContext UnitySynchronizationContext
+        {
+            get
+            {
+                if (!wasInitialized)
+                    throw new InvalidOperationException("Can't get Unity Synchronization Context before Unity initializes the syncronization.");
+                return unitySynchronizationContext;
+            }
+            private set => unitySynchronizationContext = value;
+        }
 
         /// <summary>
         /// Thread Id used by Unity main thread.
         /// </summary>
-        public static int UnityThreadId { get; private set; }
+        public static int UnityThreadId
+        {
+            get
+            {
+                if (!wasInitialized)
+                    throw new InvalidOperationException("Can't get Unity Synchronization Context before Unity initializes the syncronization.");
+                return unityThreadId;
+            }
+            private set => unityThreadId = value;
+        }
 
         /// <summary>
         /// Task Scheduler used by Unity.
         /// </summary>
-        public static TaskScheduler UnityTaskScheduler { get; private set; }
+        public static TaskScheduler UnityTaskScheduler
+        {
+            get
+            {
+                if (!wasInitialized)
+                    throw new InvalidOperationException("Can't get Unity Synchronization Context before Unity initializes the syncronization.");
+                return unityTaskScheduler;
+            }
+            private set => unityTaskScheduler = value;
+        }
 
         // https://github.com/svermeulen/Unity3dAsyncAwaitUtil/blob/master/UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
 
         private static bool wasInitialized;
+        private static SynchronizationContext unitySynchronizationContext;
+        private static int unityThreadId;
+        private static TaskScheduler unityTaskScheduler;
 
 #if UNITY_EDITOR
         [InitializeOnLoadMethod]
 #endif
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void Initialize()
         {
+            if (wasInitialized)
+                return;
             wasInitialized = true;
             UnitySynchronizationContext = SynchronizationContext.Current;
             UnityThreadId = Thread.CurrentThread.ManagedThreadId;
             UnityTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
-        }
-
-        static UnitySynchronizationContextUtility()
-        {
-            if (wasInitialized)
-                Debug.LogError("Can't get Unity Synchronization Context before Unity initializes the syncronization.");
         }
     }
 }
